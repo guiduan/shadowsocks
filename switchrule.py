da73550473ff9913291475fd75b1df54ec0c8fdc
guiduan <254732500@qq.com>
2017-03-01 17:05:11 +0800

    Update switchrule.py

--- switchrule.py
+++ switchrule.py
@@ -2,7 +2,7 @@ from configloader import load_config, get_config
 
 
 def getKeys():
-    key_list = ['id', 'port', 'u', 'd', 'transfer_enable', 'passwd', 'enable']
+    key_list = ['id', 'port', 'ku', 'kd', 'ktransfer_enable', 'passwd', 'enable']
     if get_config().API_INTERFACE == 'sspanelv3':
         key_list += ['method']
     elif get_config().API_INTERFACE == 'sspanelv3ssr':
